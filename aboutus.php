d4cf5f36b70ffb1d3ff2e18148abd00e108b061b
James24722 <103395771+James24722@users.noreply.github.com>
2022-11-24 09:44:59 +0800

    Add files via upload

---
+++ aboutus.php
@@ -0,0 +1,69 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title></title>
+<link href="default.css" rel="stylesheet" type="text/css" media="all" />
+<link href="2.css" rel="stylesheet" type="text/css" media="all" />
+<link rel="icon" href="product-images/1.ico">
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+</head>
+<body>
+	<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+<nav class="nav" id="header-wrapper">
+	<div id="header" class="container">
+		<div id="logo" >
+
+		</div>
+		<div id="qq"><h1><a href="#">Welcome to Luca's Loaves</a></h1></div>
+		<div id="logo" width: 50px></div>
+		<div id="menu">
+			<ul id="navul">
+				<li ><a href="index2.php" accesskey="1" title="">Home</a></li>
+				<li class="active"><a href="#" accesskey="2" title="">About US</a></li>
+				<li><a href="upload.php" accesskey="3" title="">Careers </a></li>
+				<li><a href="orderonline.php" accesskey="4" title="">Order online </a></li>
+				<li><a href="contactus.php" accesskey="5" title="">Contact Us</a></li>
+				<li><a href="register2.php" accesskey="6" title="">Register</a></li>
+				<div id=""></div>
+			
+			<div id="hk"></div></ul>
+		</div>
+	</div>
+</nav>
+<div class="wrapper">
+	<div id="welcome" class="container">
+		<div class="title2">
+			<h1>Key facts about Luca and the bread!</h1>
+				
+				<p>Luca commenced his career as a lifeguard but was laid off.
+                     He found he enjoyed making bread and experimented and in
+                      no time at all had built up a thriving business.</p>
+        </div>
+        <div id="three-column" class="container">
+           
+                <div class="title">
+                    <h2>About Sourdough Bread</h2>
+                
+                <ul >
+                    <li >1. It has no store/commercial yeast</li>
+                    <li>2. Hand kneaded and shaped in-house</li>
+                    <li>3. Prepared over 14-17hrs</li>
+                    <li>4. Organic flour, the water is filtered and the electricity</n>
+                         used to power the oven is solar powered!</li>
+                    <li>5. Store bought bread is mixed and baked within 2 hours meaning</n>
+                         the gluten content is really high and can lead to someone feeling</n>
+                        clogged up when eating bread. Sourdough is a great alternative</n>
+                        and much easier to digest.</li>
+                    </ul>
+                </div>
+               
+        
+		
+	</div>
+	<br><br>
+	<footer class="footer2">  
+       IT20_1 Hangzhou Qiandao Lake 203190613James
+          </footer>
+		  <script src="script2.js"></script>
+</body>
+</html>
---
+++ contactus.php
@@ -0,0 +1,57 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title></title>
+<link href="default.css" rel="stylesheet" type="text/css" media="all" />
+<link href="2.css" rel="stylesheet" type="text/css" media="all" />
+<link rel="icon" href="product-images/1.ico">
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+</head>
+<body>
+	<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+<nav class="nav" id="header-wrapper">
+	<div id="header" class="container">
+		<div id="logo" >
+
+		</div>
+		<div id="qq"><h1><a href="#">Welcome to Luca's Loaves</a></h1></div>
+		<div id="logo" width: 50px></div>
+		<div id="menu">
+			<ul id="navul">
+				<li ><a href="index2.php" accesskey="1" title="">Home</a></li>
+				<li><a href="aboutus.php" accesskey="2" title="">About US</a></li>
+				<li><a href="upload.php" accesskey="3" title="">Careers </a></li>
+				<li><a href="orderonline.php" accesskey="4" title="">Order online </a></li>
+				<li class="active"><a href="#" accesskey="5" title="">Contact Us</a></li>
+				<li><a href="register2.php" accesskey="6" title="">Register</a></li>
+				<div id=""></div>
+			
+			<div id="hk"></div></ul>
+		</div>
+	</div>
+</nav>
+<div class="wrapper">
+	<div id="welcome" class="container">
+		<div class="title">
+			<h1>Contact Us</h1>
+				
+				<p>36 Garden Ave, Mullumbimby NSW 2482 </p>
+				<a href="https://cn.bing.com/maps?&ty=18&q=36%20Garden%20Ave%2C%20Mullumbimby%2C%20New%20South%20Wales%202482%2C%20%E6%BE%B3%E6%B4%B2&ppois=-28.5508453_153.4934179_36%20Garden%20Ave%2C%20Mullumbimby%2C%20New%20South%20Wales%202482%2C%20%E6%BE%B3%E6%B4%B2_~&cp=b77c5sxsr062&lvl=16&v=2&sV=1&FORM=SNAPST" target="-blank" title="1">
+
+				<img src="map.png" width="1400" height="700">
+
+</a>
+                
+            </div>
+            </div>
+            </div>
+			<br>
+			<br>	<br>
+			<br>	<br>
+			<br>
+			<footer class="footer2">  
+        IT20_1 Hangzhou Qiandao Lake 203190613James
+          </footer>
+		  <script src="script2.js"></script>
+</body>
+</html>
